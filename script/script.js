6921291302136e65076f14335b1d3cf14ad86feb
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-11-07 13:43:29 +0200

    responsive menu

--- script/script.js
+++ script/script.js
@@ -20,4 +20,26 @@ $(document).ready(function () {
         prevArrow:'<span class="prev"><i class="fas fa-angle-left"></i></span>',
         nextArrow:false,
     });
+
+
+
+    //           бургер меню
+
+    $(function() {
+        $('.menu__icon').on('click', function() {
+            $(this).closest('.responsive_menu')
+                .toggleClass('menu_state_open');
+        });
+
+        $('.menu__links-item').on('click', function() {
+
+
+            $(this).closest('.responsive_menu')
+                .removeClass('menu_state_open');
+        });
+    });
+
+    //======================//
+
+
 })
